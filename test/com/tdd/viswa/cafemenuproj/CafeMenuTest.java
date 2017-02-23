448b6f2e2ed77e2be2dbd7e175470b2963a2f273
Viswa_workshop <Viswa_workshop@dachavaram>
2017-02-23 21:30:16 +0000

    TDD test class

---
+++ test/com/tdd/viswa/cafemenuproj/CafeMenuTest.java
@@ -0,0 +1,18 @@
+package com.tdd.viswa.cafemenuproj;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ *
+ * @author Viswa_workshop
+ */
+public class CafeMenuTest {
+    
+    public CafeMenuTest() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
